a6d0337af83e0d6b7eb4d5ba3522f22f1851a141
marcobarlo <65128997+marcobarlo@users.noreply.github.com>
2020-08-28 19:35:41 +0200

    Add files via upload

--- javaproj/Package_diagram_desktop_component/UI/Research_Tab.java
+++ javaproj/Package_diagram_desktop_component/UI/Research_Tab.java
@@ -1,16 +1,112 @@
-package Package_diagram_desktop_component.UI;
+package nulla_piu_assoluto;
 
-import Package_diagram_desktop_component.Business_Logic.*;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class Research_Tab {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class Research_Tab extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTable table;
 
 	/**
-	 * 
-	 * @param parameter
+	 * Launch the application.
 	 */
-	public void getDettagliColtivazione(Coltivazione_Business parameter) {
-		// TODO - implement Research_Tab.getDettagliColtivazione
-		throw new UnsupportedOperationException();
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Research_Tab frame = new Research_Tab();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
+	/**
+	 * Create the frame.
+	 */
+	public Research_Tab() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("tipo");
+		lblNewLabel.setBounds(34, 22, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblSezione = new JLabel("sezione");
+		lblSezione.setBounds(252, 22, 46, 14);
+		contentPane.add(lblSezione);
+		
+		JLabel lblRiga = new JLabel("riga");
+		lblRiga.setBounds(34, 55, 46, 14);
+		contentPane.add(lblRiga);
+		
+		JLabel lblPosizioneNellaRiga = new JLabel("posizione nella riga");
+		lblPosizioneNellaRiga.setBounds(194, 55, 105, 14);
+		contentPane.add(lblPosizioneNellaRiga);
+		
+		textField = new JTextField();
+		textField.setBounds(80, 52, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(80, 19, 86, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(294, 19, 86, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(294, 52, 86, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Ricerca coltivazione");
+		btnNewButton.setBounds(152, 80, 146, 14);
+		contentPane.add(btnNewButton);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 125, 414, 109);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column", "New column"
+			}
+		));
+	}
 }
