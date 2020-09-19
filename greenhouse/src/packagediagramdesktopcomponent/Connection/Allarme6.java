a7666ebec6ff74f50fadbf900b88fe098c17de63
marcobarlo <65128997+marcobarlo@users.noreply.github.com>
2020-09-19 11:17:24 +0200

    Add files via upload

--- greenhouse/src/packagediagramdesktopcomponent/Connection/Allarme6.java
+++ greenhouse/src/packagediagramdesktopcomponent/Connection/Allarme6.java
@@ -9,6 +9,6 @@ public class Allarme6 extends Allarme{
 	protected Allarme6(int idAmb,float delta)
 	{
 		super(idAmb);
-		mex="Attuatore di irradianza mal funzionante, l'irradianza attuale dista "+delta+" lux da quella target";
+		mex="Attuatore di irradianza mal funzionante, l'irradianza attuale dista "+delta+" W/mq da quella target";
 	}
 }
